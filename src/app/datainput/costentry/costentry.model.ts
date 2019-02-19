a2694de1f200f4923caf7ae6e580021b46626275
oxeed <deniss.pelna@bit.lv>
2019-02-19 16:56:57 +0200

    test

---
+++ src/app/datainput/costentry/costentry.model.ts
@@ -0,0 +1,15 @@
+export class CostEntry {
+	public id;
+	public inCat;
+	public keepMod;
+
+	constructor(id: number, inCat: any[], keepMod: any[] ){
+		this.id = id;
+		this.inCat = inCat;
+		this.keepMod = keepMod;
+	}
+}
+
+
+
+
