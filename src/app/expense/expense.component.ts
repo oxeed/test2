be4acf326e12938cdb2b67b7d50cd7571f391f79
oxeed <denis.pelna@gmail.com>
2019-03-06 10:19:27 +0200

    table view in expense component

--- src/app/expense/expense.component.ts
+++ src/app/expense/expense.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { ExpenseService } from '../expense/expense.service';
 import { CostEntryService} from '../datainput/costentry/costentry.service';
+import { ExpenseService } from '../expense/expense.service'
 
 @Component({
   selector: 'app-expense',
   templateUrl: './expense.component.html',
   styleUrls: ['./expense.component.css'],
-  providers: []
+  providers: [ExpenseService]
 })
 export class ExpenseComponent implements OnInit {
 
   expenseList;
 
-  constructor(private expenseService: CostEntryService ) { }
+  constructor(private costEntryService: CostEntryService ) { }
 
   ngOnInit() {
-    
+    this.expenseList = this.costEntryService.getCostEntry();
     console.log(this.expenseList);
   }
 
